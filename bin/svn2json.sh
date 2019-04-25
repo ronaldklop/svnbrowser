587d282ec321d8cb1ea06d1a3f70ee3cd2eb2e21
Ronald Klop <ronald@klop.ws>
2019-04-25 14:59:59 +0200

    Initial version of these files from my laptop and rpi3.
    svn2json.sh collects commits from subversion and converts them to json.
    It runs from cron:
    */11 * * * * bin/svn2json.sh 2>&1 | logger -t svn2json.sh
    
    freebsd.html is an html page reading the json files and render them to html using React.
    It has infinitiy scroll (does not work at iOS) and has a regex search field.
    It has some best effort regex replaces to make information clickable like references to man pages and PR issue numbers and commit numbers.

---
+++ bin/svn2json.sh
@@ -0,0 +1,46 @@
+#! /bin/sh
+
+. xmljson/bin/activate
+svnlite co --depth empty svn://svn.freebsd.org/base ~/src/freebsd-base
+MAX=$( svnlite log -l 1 src/freebsd-base | head -n 2 | tail -n 1 | cut -d '|' -f 1 | cut -d r -f 2 )
+STEP=100
+
+TMPDIR=/var/tmp/`basename $0`
+mkdir -p $TMPDIR
+
+START=$(( $MAX - ( ($MAX - 1) % STEP ) ))
+END=$(( $START + $STEP - 1 ))
+echo $MAX rm $TMPDIR/commits.$START-$END.json.gz
+if test -f $TMPDIR/commits.$START-$END.json.gz
+then
+  rm $TMPDIR/commits.$START-$END.json.gz
+else
+  rm $TMPDIR/commits.$(($START-$STEP))-$(($END-$STEP)).json.gz
+fi
+
+START=1
+END=$(( $START + $STEP - 1 ))
+while test $START -lt $MAX
+do
+	if ! test -f $TMPDIR/commits.$START-$END.json.gz; then
+		TMPEND=$END
+		if test $TMPEND -gt $MAX; then
+			TMPEND=$MAX
+		fi
+		svnlite log --xml -r $START:$TMPEND -v ~/src/freebsd-base > $TMPDIR/bla.xml
+		bin/xml2json.py $TMPDIR/bla.xml > $TMPDIR/commits.$START-$END.json
+		gzip -f $TMPDIR/commits.$START-$END.json
+	fi
+	START=$(( $START + $STEP ))
+	END=$(( $START + $STEP - 1 ))
+done
+echo "{" \
+	\"version\":1, \
+	\"head\":$MAX, \
+	\"start\":$(($START - $STEP)), \
+	\"step\":$STEP \
+     "}" | gzip > $TMPDIR/bla.json.gz
+scp -p -i ~/.ssh/webmaster \
+	$(find $TMPDIR -ctime -1d -name "commits.*.json.gz") \
+	$TMPDIR/bla.json.gz \
+	webmaster@klop.ws@ftp.greenhost.nl:klop.ws/www/freebsd-data/
